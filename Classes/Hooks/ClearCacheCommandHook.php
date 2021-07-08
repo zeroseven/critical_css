cc0730fc4a4ed32a11c9d6c1994d728e854cafdd
Raphael Thanner <r.thanner@zeroseven.de>
2021-07-08 23:20:51 +0200

    Limit cache commands to affected page

--- Classes/Hooks/ClearCacheCommandHook.php
+++ Classes/Hooks/ClearCacheCommandHook.php
@@ -6,15 +6,38 @@ namespace Zeroseven\CriticalCss\Hooks;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
 
 class ClearCacheCommandHook
 {
-    public function clearCachePostProc(): void
+
+    protected function getAffectedPage(array $params): int
     {
-        GeneralUtility::makeInstance(ConnectionPool::class)
+        if ($params['uid_page'] ?? null) {
+            return (int)$params['uid_page'];
+        }
+
+        if (MathUtility::canBeInterpretedAsInteger($params['cacheCmd'] ?? null)) {
+            return (int)$params['cacheCmd'];
+        }
+
+        return 0;
+    }
+
+    public function clearCachePostProc(array &$params): void
+    {
+        // Build query
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getQueryBuilderForTable('pages')
             ->update('pages')
-            ->set('critical_css_actual', 0)
-            ->execute();
+            ->set('critical_css_actual', 0);
+
+        // Limit to affected page
+        if ($pageUid = $this->getAffectedPage($params)) {
+            $queryBuilder->where($queryBuilder->expr()->eq('uid', $pageUid));
+        }
+
+        // Ciao!
+        $queryBuilder->execute();
     }
 }
