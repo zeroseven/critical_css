4baf497381d5fdf496c0a9a872b6a9ace695c86d
Raphael Thanner <r.thanner@zeroseven.de>
2021-07-15 10:49:22 +0200

    Log failed requests in sys_log table

--- Classes/Service/RequestService.php
+++ Classes/Service/RequestService.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Zeroseven\CriticalCss\Service;
 
-use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Http\Client\GuzzleClientFactory;
 use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Http\Uri;
+use TYPO3\CMS\Core\SysLog\Action as SystemLogAction;
+use TYPO3\CMS\Core\SysLog\Error as SystemLogError;
+use TYPO3\CMS\Core\SysLog\Type as SystemLogType;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use Zeroseven\CriticalCss\Middleware\UpdateStyles;
@@ -30,7 +34,7 @@ class RequestService
         return urlencode(GeneralUtility::makeInstance(UriBuilder::class)->reset()->setCreateAbsoluteUri(true)->setTargetPageUid($styles->getUid())->build());
     }
 
-    public static function send(Styles $styles): ResponseInterface
+    public static function send(Styles $styles)
     {
         $request = GeneralUtility::makeInstance(RequestFactory::class)->createRequest('get', self::URL)
             ->withHeader('X-TOKEN', SettingsService::getAuthenticationToken())
@@ -38,6 +42,16 @@ class RequestService
             ->withHeader('X-CALLBACK', self::getCallbackUrl())
             ->withHeader('X-PAGE_UID', (string)$styles->getUid());
 
-        return GuzzleClientFactory::getClient()->send($request);
+        try {
+            GuzzleClientFactory::getClient()->send($request);
+        } catch (GuzzleException $e) {
+            GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('sys_log')->insert('sys_log',[
+                'type' => SystemLogType::ERROR,
+                'action' => SystemLogAction::UNDEFINED,
+                'error' => SystemLogError::SYSTEM_ERROR,
+                'details' => $e->getMessage() . '. HTTP headers: ' . json_encode($request->getHeaders()),
+                'tstamp' => time()
+            ]);
+        }
     }
 }
