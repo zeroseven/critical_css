a7b8566c2130085cbc117bfec1708a34fad6e61f
Raphael Thanner <r.thanner@zeroseven.de>
2021-08-16 11:15:05 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,7 @@
 
 defined('TYPO3_MODE') || die('🐰');
 
-call_user_func(static function (string $_EXTKEY) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][$_EXTKEY] = \Zeroseven\CriticalCss\Hooks\PageRendererHook::class . '->postProcess';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][$_EXTKEY] = \Zeroseven\CriticalCss\Hooks\PageRendererHook::class . '->preProcess';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][$_EXTKEY] = \Zeroseven\CriticalCss\Hooks\DataHandlerHook::class . '->clearCachePostProc';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][$_EXTKEY] = \Zeroseven\CriticalCss\Hooks\ClearCacheToolbarItemHook::class;
-}, \Zeroseven\CriticalCss\Service\SettingsService::EXTENSION_KEY);
-
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][\Zeroseven\CriticalCss\Service\SettingsService::EXTENSION_KEY] = \Zeroseven\CriticalCss\Hooks\PageRendererHook::class . '->postProcess';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][\Zeroseven\CriticalCss\Service\SettingsService::EXTENSION_KEY] = \Zeroseven\CriticalCss\Hooks\PageRendererHook::class . '->preProcess';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][\Zeroseven\CriticalCss\Service\SettingsService::EXTENSION_KEY] = \Zeroseven\CriticalCss\Hooks\DataHandlerHook::class . '->clearCachePostProc';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][\Zeroseven\CriticalCss\Service\SettingsService::EXTENSION_KEY] = \Zeroseven\CriticalCss\Hooks\ClearCacheToolbarItemHook::class;
