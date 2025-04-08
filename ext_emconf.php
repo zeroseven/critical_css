a9f594c17a70d97cc8b45c22ebaaf7f199bdd3dc
Raphael Thanner <r.thanner@zeroseven.de>
2025-04-08 08:14:12 +0200

    Make it installable in TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
             'dashboard' => ''
         ]
     ]
